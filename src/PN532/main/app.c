fe09da7a219769c719e1ee45395f07851ec8310f
qitas <qitas@qitas.cn>
2022-03-02 15:55:21 +0800

    update

---
+++ src/PN532/main/app.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <esp_log.h>
+#include <esp_log_internal.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+// #include "led_strip.h"
+#include "sdkconfig.h"
+#include "pn532.h"
+
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+#define PN532_SCK CONFIG_PN532_SCK
+#define PN532_MOSI CONFIG_PN532_MOSI
+#define PN532_SS CONFIG_PN532_SS
+#define PN532_MISO CONFIG_PN532_MISO
+
+static const char *TAG = "APP";
+
+static pn532_t nfc;
+
+/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+static uint8_t s_led_state = 0;
+
+#ifdef CONFIG_BLINK_LED_RMT
+// static led_strip_t *pStrip_a;
+
+// static void blink_led(void)
+// {
+//     /* If the addressable LED is enabled */
+//     // if (s_led_state) {
+//     //     /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+//     //     pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);
+//     //     /* Refresh the strip to send data */
+//     //     pStrip_a->refresh(pStrip_a, 100);
+//     // } else {
+//     //     /* Set all LED off to clear all pixels */
+//     //     pStrip_a->clear(pStrip_a, 50);
+//     // }
+// }
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
+    /* LED strip initialization with the GPIO and pixels number*/
+    // pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
+    // /* Set all LED off to clear all pixels */
+    // pStrip_a->clear(pStrip_a, 50);
+}
+
+#elif CONFIG_BLINK_LED_GPIO
+
+// static void blink_led(void)
+// {
+//     /* Set the GPIO level according to the state (LOW or HIGH)*/
+//     gpio_set_level(BLINK_GPIO, s_led_state);
+// }
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
+    // gpio_reset_pin(BLINK_GPIO);
+    // /* Set the GPIO as a push/pull output */
+    // gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+#endif
+
+void blink_task(void *pvParameter)
+{
+    gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while (1)
+    {
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(900 / portTICK_PERIOD_MS);
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+    // configure_led();
+    // while (1) {
+    //     ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+    //     // blink_led();
+    //     // /* Toggle the LED state */
+    //     // s_led_state = !s_led_state;
+    //     vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    // }
+}
+
+void nfc_task(void *pvParameter)
+{
+    pn532_spi_init(&nfc, PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+    pn532_begin(&nfc);
+
+    uint32_t versiondata = pn532_getFirmwareVersion(&nfc);
+    if (!versiondata)
+    {
+        ESP_LOGI(TAG, "Didn't find PN53x board");
+        while (1)
+        {
+            vTaskDelay(1000 / portTICK_RATE_MS);
+        }
+    }
+    // Got ok data, print it out!
+    ESP_LOGI(TAG, "Found chip PN5 %x", (versiondata >> 24) & 0xFF);
+    ESP_LOGI(TAG, "Firmware ver. %d.%d", (versiondata >> 16) & 0xFF, (versiondata >> 8) & 0xFF);
+
+    // configure board to read RFID tags
+    pn532_SAMConfig(&nfc);
+
+    ESP_LOGI(TAG, "Waiting for an ISO14443A Card ...");
+
+    while (1)
+    {
+        uint8_t success;
+        uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0}; // Buffer to store the returned UID
+        uint8_t uidLength;                     // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+        // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+        // 'uid' will be populated with the UID, and uidLength will indicate
+        // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+        success = pn532_readPassiveTargetID(&nfc, PN532_MIFARE_ISO14443A, uid, &uidLength, 0);
+
+        if (success)
+        {
+            // Display some basic information about the card
+            ESP_LOGI(TAG, "Found an ISO14443A card");
+            ESP_LOGI(TAG, "UID Length: %d bytes", uidLength);
+            ESP_LOGI(TAG, "UID Value:");
+            esp_log_buffer_hexdump_internal(TAG, uid, uidLength, ESP_LOG_INFO);   
+            vTaskDelay(1000 / portTICK_RATE_MS);         
+        }
+        else
+        {
+            // PN532 probably timed out waiting for a card
+            ESP_LOGI(TAG, "Timed out waiting for a card");
+        }
+    }
+}
+
+void app_main()
+{
+    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+    xTaskCreate(&nfc_task, "nfc_task", 4096, NULL, 4, NULL);
+}
