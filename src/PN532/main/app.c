5f5057a22a82f3fa1e0974c82c92f7692da7b2af
qitas <qitas@qitas.cn>
2022-03-02 18:20:48 +0800

    update

--- src/PN532/main/app.c
+++ src/PN532/main/app.c
@@ -8,19 +8,18 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
-// #include "led_strip.h"
+
+#include "led_strip.h"
 #include "sdkconfig.h"
+#include "esp_log.h"
 #include "pn532.h"
 
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define PN532_SCK   CONFIG_PN532_SCK
+#define PN532_MOSI  CONFIG_PN532_MOSI
+#define PN532_SS    CONFIG_PN532_SS
+#define PN532_MISO  CONFIG_PN532_MISO
 
-#define PN532_SCK CONFIG_PN532_SCK
-#define PN532_MOSI CONFIG_PN532_MOSI
-#define PN532_SS CONFIG_PN532_SS
-#define PN532_MISO CONFIG_PN532_MISO
-
-static const char *TAG = "APP";
+static const char *TAG = "PN";
 
 static pn532_t nfc;
 
@@ -32,48 +31,48 @@ static pn532_t nfc;
 static uint8_t s_led_state = 0;
 
 #ifdef CONFIG_BLINK_LED_RMT
-// static led_strip_t *pStrip_a;
+static led_strip_t *pStrip_a;
 
-// static void blink_led(void)
-// {
-//     /* If the addressable LED is enabled */
-//     // if (s_led_state) {
-//     //     /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-//     //     pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);
-//     //     /* Refresh the strip to send data */
-//     //     pStrip_a->refresh(pStrip_a, 100);
-//     // } else {
-//     //     /* Set all LED off to clear all pixels */
-//     //     pStrip_a->clear(pStrip_a, 50);
-//     // }
-// }
+static void blink_led(void)
+{
+    /* If the addressable LED is enabled */
+    if (s_led_state) {
+        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+        pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);
+        /* Refresh the strip to send data */
+        pStrip_a->refresh(pStrip_a, 100);
+    } else {
+        /* Set all LED off to clear all pixels */
+        pStrip_a->clear(pStrip_a, 50);
+    }
+}
 
 static void configure_led(void)
 {
     ESP_LOGI(TAG, "Example configured to blink addressable LED!");
     /* LED strip initialization with the GPIO and pixels number*/
-    // pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
-    // /* Set all LED off to clear all pixels */
-    // pStrip_a->clear(pStrip_a, 50);
+    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
+    /* Set all LED off to clear all pixels */
+    pStrip_a->clear(pStrip_a, 50);
 }
 
 #elif CONFIG_BLINK_LED_GPIO
 
-// static void blink_led(void)
-// {
-//     /* Set the GPIO level according to the state (LOW or HIGH)*/
-//     gpio_set_level(BLINK_GPIO, s_led_state);
-// }
+static void blink_led(void)
+{
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
 
 static void configure_led(void)
 {
     ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    // gpio_reset_pin(BLINK_GPIO);
-    // /* Set the GPIO as a push/pull output */
-    // gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
-
 #endif
+
 
 void blink_task(void *pvParameter)
 {
