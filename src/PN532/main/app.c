a4a4bccef4f64bdec62a54db5719e4b1ec6c459d
qitas <qitas@qitas.cn>
2022-03-02 19:09:04 +0800

    update

--- src/PN532/main/app.c
+++ src/PN532/main/app.c
@@ -8,7 +8,6 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
-
 #include "led_strip.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
@@ -19,82 +18,16 @@
 #define PN532_SS    CONFIG_PN532_SS
 #define PN532_MISO  CONFIG_PN532_MISO
 
-static const char *TAG = "PN";
+static const char *TAG = "PN532";
 
 static pn532_t nfc;
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
-
-static uint8_t s_led_state = 0;
-
-#ifdef CONFIG_BLINK_LED_RMT
-static led_strip_t *pStrip_a;
-
-static void blink_led(void)
-{
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);
-        /* Refresh the strip to send data */
-        pStrip_a->refresh(pStrip_a, 100);
-    } else {
-        /* Set all LED off to clear all pixels */
-        pStrip_a->clear(pStrip_a, 50);
-    }
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
-    /* LED strip initialization with the GPIO and pixels number*/
-    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
-    /* Set all LED off to clear all pixels */
-    pStrip_a->clear(pStrip_a, 50);
-}
-
-#elif CONFIG_BLINK_LED_GPIO
-
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-#endif
-
-
-void blink_task(void *pvParameter)
-{
-    gpio_pad_select_gpio(BLINK_GPIO);
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while (1)
-    {
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(900 / portTICK_PERIOD_MS);
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-    // configure_led();
-    // while (1) {
-    //     ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-    //     // blink_led();
-    //     // /* Toggle the LED state */
-    //     // s_led_state = !s_led_state;
-    //     vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-    // }
-}
-
+/*******************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：无
+**输出参数 ：无
+********************************************************************************/
 void nfc_task(void *pvParameter)
 {
     pn532_spi_init(&nfc, PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
@@ -135,8 +68,8 @@ void nfc_task(void *pvParameter)
             ESP_LOGI(TAG, "Found an ISO14443A card");
             ESP_LOGI(TAG, "UID Length: %d bytes", uidLength);
             ESP_LOGI(TAG, "UID Value:");
-            esp_log_buffer_hexdump_internal(TAG, uid, uidLength, ESP_LOG_INFO);   
-            vTaskDelay(1000 / portTICK_RATE_MS);         
+            esp_log_buffer_hexdump_internal(TAG, uid, uidLength, ESP_LOG_INFO);
+            vTaskDelay(1000 / portTICK_RATE_MS);
         }
         else
         {
@@ -145,9 +78,14 @@ void nfc_task(void *pvParameter)
         }
     }
 }
-
+/*******************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：无
+**输出参数 ：无
+********************************************************************************/
 void app_main()
 {
-    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+    // xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
     xTaskCreate(&nfc_task, "nfc_task", 4096, NULL, 4, NULL);
 }
